dc2ffbd51243785738d29fa2af31bdd1bd2f6a33
hanyue1014 <wongxuanbin@gmail.com>
2024-05-08 22:36:34 +0800

    Add validation utility and extract media upload

--- CursusVia/Controls/FileUploadWithPreview.ascx.cs
+++ CursusVia/Controls/FileUploadWithPreview.ascx.cs
@@ -5,17 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace CursusVia.Tutor
+namespace CursusVia.Controls
 {
-    public partial class CreateCourse : System.Web.UI.Page
+    public partial class FileUploadWithPreview : System.Web.UI.UserControl
     {
+        public bool IsRequired { get; set; }
+        public FileUpload MediaFileUpload { get { return _MediaFileUpload; } }
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void submitForm_Click(object sender, EventArgs e)
-        {
         }
     }
 }
---
+++ CursusVia/InputWithValidator.js
@@ -0,0 +1,13 @@
+﻿// use mutation observer to listen for style changes cuz dotnet with partial postback wont reexecute js, so cannot just loop through the list of elements and check style, this js file is only executed once when the page loads for the first time, so instead, listen for changes in style to detect the appearance of the validation message
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutationRecord => {
+        console.log(mutationRecord, mutationRecord.target.style.visibility);
+        if (mutationRecord.target.style.visibility === 'visible')
+            mutationRecord.target.parentElement.classList.add('validationError')
+    });    
+});
+
+const targets = document.querySelectorAll('.validationMessage');
+targets.forEach(target => {
+    observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+})
