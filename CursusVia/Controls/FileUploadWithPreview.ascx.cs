c1070b0de228d7a26cd48a6e471ac49041a5bc2e
hanyue1014 <wongxuanbin@gmail.com>
2024-05-10 22:05:45 +0800

    Add new file

--- CursusVia/Controls/FileUploadWithPreview.ascx.cs
+++ CursusVia/Controls/FileUploadWithPreview.ascx.cs
@@ -12,6 +12,7 @@ namespace CursusVia.Controls
         public bool IsRequired { get; set; }
         public FileUpload MediaFileUpload { get { return _MediaFileUpload; } }
         public string Src { get; set; }
+        public string FileType { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
 
---
+++ CursusVia/Controls/MultiFileUpload.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MultiFileUpload.ascx.cs" Inherits="CursusVia.Controls.MultiFileUpload" %>
