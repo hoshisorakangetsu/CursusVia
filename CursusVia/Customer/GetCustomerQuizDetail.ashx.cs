b0d0ffbb56ff5f189aecae85d77deb25d352725a
hanyue1014 <wongxuanbin@gmail.com>
2024-05-12 22:12:30 +0800

    Add new file

---
+++ CursusVia/Customer/GetCustomerQuizDetail.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursusVia.Customer
+{
+    /// <summary>
+    /// Summary description for GetCustomerQuizDetail
+    /// </summary>
+    public class GetCustomerQuizDetail : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
